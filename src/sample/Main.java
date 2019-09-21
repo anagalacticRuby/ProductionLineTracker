9b71e119eb085e5837456fec8e87c8502241f7e6
anagalacticRuby <841b2dRuby>
2019-09-21 17:25:46 -0400

    Fixed database so it's properly attached and formatted, cleaned up some warnings.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 /**
  * Yeah bro this is the main file of the production line program.
  *
@@ -21,15 +23,52 @@ public class Main extends Application {
    */
   @Override
   public void start(Stage primaryStage) throws Exception {
+
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
     primaryStage.setTitle("WOW man WELCOME to the PRODUCTION LINE TRACKER PROGRAM");
     primaryStage.setScene(new Scene(root, 600, 400));
     primaryStage.show();
     // ctrl+alt+L is the google java formatter
     System.out.println("IM SUPER RAD");
+
   }
 
   public static void main(String[] args) {
     launch(args);
+    //make sure IntelliJ isn't connected to database when running this program
+    //click the red square before running program if it's light up.
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./prod/PRODUCT";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    //Don't publish database password to Github
+    //Modify before you push for security reasons
+    Connection conn;
+    //Connection is a class and conn is an instance of the class (AKA object)
+    Statement stmt;
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+
+      String sqlSelectAllProducts = "SELECT * FROM Product";
+      ResultSet rs = stmt.executeQuery(sqlSelectAllProducts);
+      // STEP 4: Clean-up environment
+
+      rs.close();
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+
+    }
   }
+
 }
