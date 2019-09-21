17b195b29559ff1fdd79363c744db8867bb66004
anagalacticRuby <841b2dRuby>
2019-09-21 15:21:58 -0400

    added so much stuff it's super good now c:

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,66 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+  @FXML private TabPane tbpTabPane;
+
+  @FXML private Tab tabProductLine;
+
+  @FXML private Label lblProductName;
+
+  @FXML private Label lblManufacturer;
+
+  @FXML private Label lblItemType;
+
+  @FXML private TextField txtProductName;
+
+  @FXML private TextField txtManufacturer;
+
+  @FXML private ChoiceBox<?> chbItemType;
+
+  @FXML private Button btnAddProduct;
+
+  @FXML private TableView<?> tblExistingProducts;
+
+  @FXML private Label lblExistingProducts;
+
+  @FXML private Tab tabProduce;
+
+  @FXML private Label lblChooseProduct;
+
+  @FXML private ListView<?> listChooseProduct;
+
+  @FXML private Label lblChooseQuant;
+
+  @FXML private ComboBox<?> comboChooseQuant;
+
+  @FXML private Button btnRecordProduction;
+
+  @FXML private Tab tabProductionLine;
+
+  @FXML private TextArea txaProductLog;
+
+  @FXML
+  void addProduct(MouseEvent event) {
+    System.out.println("ADDED");
+  }
+
+  @FXML
+  void recordProduction(ActionEvent event) {
+    System.out.println("RECORDED");
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Yeah bro this is the main file of the production line program.
+ *
+ * @author Nicholas H
+ */
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  /**
+   * The starting point of a JavaFX program.
+   *
+   * @param primaryStage the first thing the user sees
+   * @throws Exception baba
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("WOW man WELCOME to the PRODUCTION LINE TRACKER PROGRAM");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+    // ctrl+alt+L is the google java formatter
+    System.out.println("IM SUPER RAD");
+  }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
